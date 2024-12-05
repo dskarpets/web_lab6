eeee92f6db45c7e1d891a94ce23ac93283cd716e
Andre <malii.andrii@lll.kpi.ua>
2024-12-05 22:36:51 +0200

    init

---
+++ save_menu.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = file_get_contents('php://input');
+    file_put_contents('menu.json', $data);
+    echo json_encode(["status" => "success"]);
+}
+?>
