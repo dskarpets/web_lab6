0f577fbb882657e0e7c8dac0f7ebb9b3f1d01eba
Andre <malii.andrii@lll.kpi.ua>
2024-12-05 23:20:50 +0200

    init

--- save_menu.php
+++ save_menu.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = file_get_contents('php://input');
-    file_put_contents('menu.json', $data); // Зберігаємо у JSON-файлі
+    file_put_contents('menu.json', $data);
     echo json_encode(['status' => 'success']);
 } else {
     echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
